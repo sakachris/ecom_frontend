381027108c302f74719fa54902780588be1ac79f
sakachris <sakachris90@gmail.com>
2025-11-27 18:25:45 +0300

    deploy: adding Dockerfile and github action for building and pushing image to docker hub

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -1,8 +1,9 @@
+// src/components/auth/AuthModal.tsx
+
 "use client";
 
 import React, { useState } from "react";
 import SignInForm from "./SignInForm";
-// import SignUpForm from "./SignUpForm";
 import RegisterForm from "./RegisterForm";
 
 export default function AuthModal({
@@ -13,7 +14,6 @@ export default function AuthModal({
   onClose: () => void;
 }) {
   const [mode, setMode] = useState<"login" | "register">("login");
-  //   const [infoMessage, setInfoMessage] = useState(null);
   const [infoMessage, setInfoMessage] = useState<string | null>(null);
   const [registeredEmail, setRegisteredEmail] = useState<string | null>(null);
 
@@ -37,23 +37,16 @@ export default function AuthModal({
         </div>
 
         {mode === "login" ? (
-          //   <SignInForm infoMessage={infoMessage} onSuccess={onClose} />
           <SignInForm
             infoMessage={infoMessage}
             onSuccess={onClose}
             prefilledEmail={registeredEmail}
           />
         ) : (
-          //   <RegisterForm
-          //     onSuccess={(msg) => {
-          //       setInfoMessage(msg);
-          //       setMode("login");
-          //     }}
-          //   />
           <RegisterForm
             onSuccess={(msg, email) => {
               setInfoMessage(msg);
-              setRegisteredEmail(email); // ⭐ store the email
+              setRegisteredEmail(email);
               setMode("login");
             }}
           />
@@ -62,7 +55,7 @@ export default function AuthModal({
         <div className="mt-4 text-sm text-gray-600 text-center">
           {mode === "login" ? (
             <>
-              Don’t have an account?{" "}
+              Don`t have an account?{" "}
               <button
                 className="text-black underline"
                 onClick={() => setMode("register")}
@@ -86,51 +79,3 @@ export default function AuthModal({
     </div>
   );
 }
-
-// // src/components/auth/AuthModal.tsx
-// "use client";
-
-// import React from "react";
-// import SignInForm from "./SignInForm";
-
-// export default function AuthModal({
-//   open,
-//   onClose,
-// }: {
-//   open: boolean;
-//   onClose: () => void;
-// }) {
-//   if (!open) return null;
-
-//   return (
-//     <div
-//       className="fixed inset-0 z-50 flex items-center justify-center"
-//       role="dialog"
-//       aria-modal="true"
-//     >
-//       <div
-//         className="fixed inset-0 bg-black/40"
-//         onClick={onClose}
-//         aria-hidden="true"
-//       />
-
-//       <div className="relative z-50 w-full max-w-md mx-4 bg-white rounded-lg shadow-lg p-6">
-//         <div className="flex items-center justify-between mb-4">
-//           <h3 className="text-lg font-semibold">Sign in to ELECTROCO</h3>
-//           <button
-//             onClick={onClose}
-//             className="text-gray-500 hover:text-gray-700"
-//           >
-//             ✕
-//           </button>
-//         </div>
-
-//         <SignInForm onSuccess={onClose} />
-
-//         <div className="mt-4 text-sm text-gray-600">
-//           Don’t have an account? <a className="text-black underline">Sign up</a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
